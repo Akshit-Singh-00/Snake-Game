04b107aff9918341af97026789a95a36593876c0
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-08 17:31:21 +0530

    tried to include the basic snake fucntionality using JS

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const blockWidth = 50;
 const cols = Math.floor(board.clientWidth / blockHeight);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-const blocks=[];
+const blocks={};
 const snake=[{
     x:1,y:3
-},{
-    x:1,y:4
-},{
-    x:1,y:5
 }];
+
+let direction='down'
+
 // for(let i=0;i<rows*cols;i++){
 //     const block=document.createElement('div');
 //     block.classList.add("block");
@@ -27,3 +26,54 @@ for(let row=0;row<rows;row++){
     blocks[`${row}-${col}`]=block
     }
 }
+
+function render() {
+    snake.forEach(segment => {
+        const block = blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+
+    });
+}
+
+setInterval(() => {
+    let head=null
+
+    if(direction=="left"){
+        head={x:snake[0].x,y:snake[0].y-1}
+    }
+    else if(direction=="right"){
+        head={x:snake[0].x,y:snake[0].y+1}
+    }
+    else if(direction=="down"){
+        head={x:snake[0].x+1,y:snake[0].y}
+    }
+    else if(direction=="up"){
+        head={x:snake[0].x-1,y:snake[0].y}
+    }
+
+    snake.forEach(segment=>{
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+
+  snake.unshift(head)
+  snake.pop()
+
+    render()
+}, 350);
+
+
+addEventListener("keydown",(event)=>{
+    // console.log(event.key);
+    if(event.key=="ArrowUp"){
+        direction="up"
+    }
+    else if(event.key=="ArrowRight"){
+        direction="right"
+    }
+    else if(event.key=="ArrowLeft"){
+        direction="left"
+    }
+    else if(event.key=="ArrowDown"){
+        direction="down"
+    }
+
+})
