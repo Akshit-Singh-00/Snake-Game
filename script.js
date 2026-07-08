7c04f81c4cc3f15f2da5dd9735f48e5f12fb6a9f
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-08 16:51:48 +0530

    tried to create grid with proper cordinates using JS & CSS

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 50;
+const blockWidth = 50;
 const cols = Math.floor(board.clientWidth / blockHeight);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
+const blocks=[];
+const snake=[{
+    x:1,y:3
+},{
+    x:1,y:4
+},{
+    x:1,y:5
+}];
+// for(let i=0;i<rows*cols;i++){
+//     const block=document.createElement('div');
+//     block.classList.add("block");
+//     board.appendChild(block);
+// }
 
-for(let i=0;i<rows*cols;i++){
-    const block=document.createElement('div');
+for(let row=0;row<rows;row++){
+    for(let col=0;col<cols;col++){
+            const block=document.createElement('div');
     block.classList.add("block");
     board.appendChild(block);
+    block.innerText=`${row}-${col}`
+    blocks[`${row}-${col}`]=block
+    }
 }
